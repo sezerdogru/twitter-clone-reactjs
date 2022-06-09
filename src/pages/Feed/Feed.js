9c55f7bd130668e1f7fa674228717d303dc7dbe4
sezerdogru <sezer.dogru91@gmail.com>
2022-06-09 22:01:23 +0300

    Add  Post

--- src/pages/Feed/Feed.js
+++ src/pages/Feed/Feed.js
@@ -1,6 +1,7 @@
 import React from "react"
 import './Feed.css'
 import Tweetbox from "./Tweetbox"
+import Post from "./Post"
 
 function Feed() {
 	return(
@@ -9,6 +10,15 @@ function Feed() {
 				<h2>Home</h2>
 			</div> 
 			<Tweetbox />
+
+			<Post 
+				displayName="Sezer Doğru"
+				username="sezerdogru"
+				verified={true}
+				text="yazı"
+				image="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg"
+				avatar="https://pbs.twimg.com/profile_images/1506021070666612737/9nYOxDqS_400x400.jpg"
+			/>
 		</div>
 	)
 }
